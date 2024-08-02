710cd219cfde596a3a11fc29485815dc540f3eab
0mgRod <rodrigofonseca20@outlook.com>
2024-08-02 22:45:11 +0100

    quick update that dont work yet

--- src/main.cpp
+++ src/main.cpp
@@ -21,44 +21,53 @@ $execute {
 
 static ImFont* openSans = nullptr;
 
-static void createAddressLabelWithCopyButton(const char* label) {
-    ImGui::Text("%s: %s");
-    ImGui::SameLine();
-    if (ImGui::Button(fmt::format("Copy##{}", label).c_str()));
+static void createCheckbox(const char* label, bool* enabled, const char* tag) {
+    if (ImGui::Checkbox(label, enabled)) {
+        printf("Checkbox '%s' changed to %s\n", tag, *enabled ? "true" : "false");
+    }
+}
+
+static void createIntValue(const char* label, int* value, const char* tag) {
+    if (ImGui::InputInt(label, value)) {
+        printf("Int value '%s' changed to %d\n", tag, *value);
+    }
 }
 
 $execute {
-	ImGuiCocos::get()
-		.setup([]{
-			openSans = ImGui::GetIO().Fonts->AddFontFromFileTTF((Mod::get()->getResourcesDir() / "opensans.ttf").string().c_str(), 20.0f);
-		})
-		.draw([]{
-			if (ImGui::Begin("Player")) {
+    ImGuiCocos::get()
+        .setup([]{
+            openSans = ImGui::GetIO().Fonts->AddFontFromFileTTF((Mod::get()->getResourcesDir() / "opensans.ttf").string().c_str(), 20.0f);
+        })
+        .draw([]{
+            if (ImGui::Begin("Player")) {
                 ImGui::PushFont(openSans);
 
-                createAddressLabelWithCopyButton("Geometry Dash Base");
-                createAddressLabelWithCopyButton("Cocos2d-x Base");
-                ImGui::NewLine();
-                createAddressLabelWithCopyButton("AchievementManager");
-                createAddressLabelWithCopyButton("AchievementNotifier");
+                static bool noclipEnabled = Mod::get()->getSavedValue<bool>("noclip");
+                createCheckbox("Noclip", &noclipEnabled, "noclip");
 
-                if (ImGui::Button("Close")) ImGuiCocos::get().setVisible(false);
+                ImGui::PopFont();
+            }
+			if (ImGui::Begin("Global")) {
+                ImGui::PushFont(openSans);
+
+                static int speedHackValue = Mod::get()->getSavedValue<int>("speedhack");
+                createIntValue("Speedhack", &speedHackValue, "speedhack");
 
                 ImGui::PopFont();
             }
             ImGui::End();
-		})
-		.setVisible(false);
+        })
+        .setVisible(false);
 }
 
 class $modify(MyCCScene, CCScene) {
 public:
-	virtual bool init() {
-		this->template addEventListener<InvokeBindFilter>([=](InvokeBindEvent* event) {
+    virtual bool init() {
+        this->template addEventListener<InvokeBindFilter>([=](InvokeBindEvent* event) {
             if (event->isDown()) {
                 ImGuiCocos::get().toggle();
             }
             return ListenerResult::Propagate;
         }, "open-modmenu"_spr);
-	}
+    }
 };
