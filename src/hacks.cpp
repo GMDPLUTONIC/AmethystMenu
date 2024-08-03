f6259c90c0a7180b91d7818527ce99de3036c54f
OmgRod <89850217+OmgRod@users.noreply.github.com>
2024-08-03 10:25:24 +0100

    Update hacks.cpp

--- src/hacks.cpp
+++ src/hacks.cpp
@@ -4,6 +4,6 @@ bool hacks::isHackEnabled(char* tag) {
     return Mod::get()->getSavedValue<bool>(tag);
 }
 
-void Hacks::setHackEnabled(char* tag, bool enabled) {
+void hacks::setHackEnabled(char* tag, bool enabled) {
     Mod::get()->setSavedValue<bool>(tag, enabled);
 }
