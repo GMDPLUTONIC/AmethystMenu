4b880171414ae319fb2c2133fba3d47aa8fed6d3
OmgRod <89850217+OmgRod@users.noreply.github.com>
2024-08-06 12:45:27 +0100

    Update Speedhack.cpp

--- src/hacks/global/Speedhack.cpp
+++ src/hacks/global/Speedhack.cpp
@@ -1,7 +1,95 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/PlayLayer.hpp>
-#include "../../hacks.hpp"
+#include <Geode/modify/CCScheduler.hpp>
+#include "Speedhack.hpp"
 
 using namespace geode::prelude;
 
-// hacks::getInstance().getIntValue("speedhack");
+FMOD::ChannelGroup* masterGroup;
+
+float speedhackLogic(float dt)
+{
+    #ifndef GEODE_IS_IOS
+    if (!masterGroup)
+        FMODAudioEngine::sharedEngine()->m_system->getMasterChannelGroup(&masterGroup);
+    #endif
+
+    if (!masterGroup)
+        return dt;
+
+    float speedMultiplier = hacks::getInstance().getIntValue("speedhack");
+
+    if (speedMultiplier != 1)
+    {
+        #ifdef GEODE_IS_IOS
+        reinterpret_cast<FMOD_RESULT(__cdecl*)(FMOD::ChannelControl*, float)>(geode::base::get() + 0x4ffec4)(masterGroup, speedMultiplier);
+        #else
+        masterGroup->setPitch(speedMultiplier);
+        #endif
+        ColourUtility::update(dt * speedMultiplier);
+        return dt * speedMultiplier;
+    }
+
+    #ifdef GEODE_IS_IOS
+    reinterpret_cast<FMOD_RESULT(__cdecl*)(FMOD::ChannelControl*, float)>(geode::base::get() + 0x4ffec4)(masterGroup, 1);
+    #else
+    masterGroup->setPitch(1);
+    #endif
+    ColourUtility::update(dt);
+    return dt;
+}
+
+#ifdef GEODE_IS_MACOS
+
+class $modify (CCScheduler)
+{
+    virtual void update(float dt)
+    {
+        dt = speedhackLogic(dt);
+        CCScheduler::update(dt);
+    }
+};
+
+#else
+
+void myUpdate(CCScheduler* ins, float dt)
+{
+    dt = speedhackLogic(dt);
+    ins->update(dt);
+}
+
+$execute {
+    Mod::get()->hook(
+        reinterpret_cast<void*>(
+            geode::addresser::getVirtual(&CCScheduler::update)
+        ),
+        &myUpdate,
+        "cocos2d::CCScheduler::update",
+        tulip::hook::TulipConvention::Thiscall
+    );
+}
+
+#endif
+
+#ifdef GEODE_IS_IOS
+
+FMOD_RESULT FMOD_System_createChannelGroup(FMOD::System* self, const char *name, FMOD::ChannelGroup **channelgroup) {
+    auto res = reinterpret_cast<FMOD_RESULT(__cdecl*)(FMOD::System*, const char*, FMOD::ChannelGroup**)>(geode::base::get() + 0x4c8964)(self, name, channelgroup);
+
+    if (!masterGroup)
+        masterGroup = *channelgroup;
+
+    log::info("WE HAVE A MASTER GROUP LETS GO!");
+
+    return res;
+}
+
+$execute {
+    Mod::get()->hook(
+        reinterpret_cast<void*>(geode::base::get() + 0x4c8964), 
+        &FMOD_System_createChannelGroup, 
+        "FMOD::System::createChannelGroup", 
+        tulip::hook::TulipConvention::Cdecl 
+    );
+}
+
+#endif
