7ece2b40d341a8ab013f5abfe8bae5df76581539
OmgRod <89850217+OmgRod@users.noreply.github.com>
2024-08-06 12:14:04 +0200

    Update CharacterFilter.cpp

--- src/hacks/bypass/CharacterFilter.cpp
+++ src/hacks/bypass/CharacterFilter.cpp
@@ -6,12 +6,12 @@ using namespace geode::prelude;
 #include <Geode/modify/CCTextInputNode.hpp>
 
 class $modify(CCTextInputNode) {
-    void updateLabel(gd::string p0) {
+    void updateLabel(std::string p0) {
         if (hacks::getInstance().isHackEnabled("character")) {
             this->setAllowedChars("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,-!?:;)(/\\\"\'`*= +-_%[]<>|@&^#{}%$~");
             CCTextInputNode::updateLabel(p0);
         } else {
             CCTextInputNode::updateLabel(p0);
-        {
-    };
+        }
+    }
 };
