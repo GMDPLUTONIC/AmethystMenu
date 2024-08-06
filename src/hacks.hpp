6b7455e05ea80bdabf157fe48cead91592cf1995
OmgRod <89850217+OmgRod@users.noreply.github.com>
2024-08-06 14:00:56 +0100

    Update hacks.hpp

--- src/hacks.hpp
+++ src/hacks.hpp
@@ -12,9 +12,11 @@ public:
 
     bool isHackEnabled(const std::string& tag);
     int getIntValue(const std::string& tag);
+    float getFloatValue(const std::string& tag);
     void setHackEnabled(const std::string& tag, bool enabled);
     bool hackValueExists(const std::string& tag);
     void setHackIntValue(const std::string& tag, int value);
+    void setHackFloatValue(const std::string& tag, float value);
 
 private:
     // Private constructor to prevent instantiation
