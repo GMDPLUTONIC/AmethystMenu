0465fe32e4960767dfad0c6513a752356d38593e
OmgRod <89850217+OmgRod@users.noreply.github.com>
2024-08-06 14:02:44 +0100

    Update hacks.cpp

--- src/hacks.cpp
+++ src/hacks.cpp
@@ -8,12 +8,20 @@ int hacks::getIntValue(const std::string& tag) {
     return Mod::get()->getSavedValue<int>(tag);
 }
 
+float hacks::getFloatValue(const std::string& tag) {
+    return Mod::get()->getSavedValue<float>(tag);
+}
+
 void hacks::setHackEnabled(const std::string& tag, bool enabled) {
     Mod::get()->setSavedValue<bool>(tag, enabled);
 }
 
 void hacks::setHackIntValue(const std::string& tag, int value) {
     Mod::get()->setSavedValue<int>(tag, value);
+}
+
+void hacks::setHackFloatValue(const std::string& tag, float value) {
+    Mod::get()->setSavedValue<float>(tag, value);
 }
 
 bool hacks::hackValueExists(const std::string& tag) {
